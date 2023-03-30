ae55cff848dd6df5fc1456c20ae189b630281fa9
PavelPoleshchuk <ppl@vivanta.by>
2023-03-30 12:32:56 +0300

    fix: delete comments

--- first-app/src/pages/formPage/FormPage.tsx
+++ first-app/src/pages/formPage/FormPage.tsx
@@ -1,22 +1,3 @@
-// import { CardList } from 'components/cardList/CardList';
-// import { Form } from 'components/Form/Form';
-// import { ICards } from 'components/types/types';
-// import React, { Component } from 'react';
-
-// interface IState {
-//   cardsData: ICards[];
-// }
-
-// class FormPage extends Component<object, IState> {
-//   state: IState = {
-//     cardsData: [],
-//   };
-
-//   addNewCard = (newCard: ICards) => {
-//     this.setState(({ cardsData }) => ({
-//       cardsData: [...cardsData, newCard],
-//     }));
-//   };
 import { CardList } from 'components/cardList/CardList';
 import { Form } from 'components/Form/Form';
 import { ICards } from 'components/types/types';
@@ -39,16 +20,3 @@ const FormPage = () => {
 };
 
 export default FormPage;
-
-//   render() {
-//     return (
-//       <div className="top-block">
-//         <span className="page-number-span">page: Forms</span>
-//         <Form addNewCard={this.addNewCard} />
-//         <CardList cardsData={this.state.cardsData} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default FormPage;
